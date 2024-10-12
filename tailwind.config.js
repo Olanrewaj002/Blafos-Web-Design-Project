16ccf195b580dad7e436988e2419e7d773751272
Mac <user@Macs-MacBook-Pro.local>
2024-10-12 07:01:52 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      spacing: {
+        '13': '3.25rem',
+        '15': '3.75rem',
+        '128': '32rem',
+        '144': '36rem',
+        '168': '52rem',
+      }
+      }
+    },
+  plugins: [],
+}
+
